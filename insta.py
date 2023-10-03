f9c6b03e246211f22eef300eb186c3f8b483bc54
Spacedreameriscoding <faustinvollereaux@gmail.com>
2023-10-03 20:23:15 +0200

    Update insta.py

--- insta.py
+++ insta.py
@@ -15,6 +15,7 @@ def get_info(username):
         for i in range(10):
             if account.text[location+i].isdigit():
                 result = result + account.text[location+i]
+                result = int(result)
     return result
 
 # get info function: get how many post does an instagram account made
